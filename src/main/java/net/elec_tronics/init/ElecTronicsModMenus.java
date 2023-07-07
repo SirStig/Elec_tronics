67a69121b9304da259c1d0a94207bfb77f462575
Joshua <twogoodgamer2@gmail.com>
2023-07-07 15:15:40 -0600

    push: Pushing changes

--- src/main/java/net/elec_tronics/init/ElecTronicsModMenus.java
+++ src/main/java/net/elec_tronics/init/ElecTronicsModMenus.java
@@ -12,6 +12,7 @@ import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraft.world.inventory.MenuType;
 
 import net.elec_tronics.world.inventory.WorkbenchguiMenu;
+import net.elec_tronics.world.inventory.TankT1GuiMenu;
 import net.elec_tronics.world.inventory.SolarpanelGUIMenu;
 import net.elec_tronics.world.inventory.ExtrusinmachineGUIMenu;
 import net.elec_tronics.world.inventory.CrusherT1GUIMenu;
@@ -27,4 +28,5 @@ public class ElecTronicsModMenus {
 	public static final RegistryObject<MenuType<CrusherT1GUIMenu>> CRUSHER_T_1_GUI = REGISTRY.register("crusher_t_1_gui", () -> IForgeMenuType.create(CrusherT1GUIMenu::new));
 	public static final RegistryObject<MenuType<SolarpanelGUIMenu>> SOLARPANEL_GUI = REGISTRY.register("solarpanel_gui", () -> IForgeMenuType.create(SolarpanelGUIMenu::new));
 	public static final RegistryObject<MenuType<ExtrusinmachineGUIMenu>> EXTRUSINMACHINE_GUI = REGISTRY.register("extrusinmachine_gui", () -> IForgeMenuType.create(ExtrusinmachineGUIMenu::new));
+	public static final RegistryObject<MenuType<TankT1GuiMenu>> TANK_T_1_GUI = REGISTRY.register("tank_t_1_gui", () -> IForgeMenuType.create(TankT1GuiMenu::new));
 }
